597c8462000df978e112109000f8cf6158cd0fa1
Chinnon Santos <chinnonsantos@gmail.com>
2023-01-17 14:29:32 -0300

    feat: implementing BinarySearch Class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,8 @@ fun main() {
     val sortedIntList: List<Int> = List(LIST_MAX_SIZE) { it + 1 }
 
     val linearSearch = LinearSearch(sortedList = sortedIntList, search = desiredNumber)
-    println("Hello World!")
+    val binarySearch = BinarySearch(sortedList = sortedIntList, search = desiredNumber)
+
     println("Desired number: $desiredNumber")
     println("List size: ${sortedIntList.size}")
 
@@ -20,4 +21,8 @@ fun main() {
     println("\n### Linear Search - Brute Force Approach (For loop) ###")
     val howSlowIsBruteForceFor = measureNanoTime{ linearSearch.byBruteForceUsingForLoop() }
     println("took ${howSlowIsBruteForceFor / 1000} microseconds to execute!")
+
+    println("\n### Binary Search - Divide And Conquer Approach (Recursive loop) ###")
+    val howSlowIsDivideAndConquer = measureNanoTime{ binarySearch.byDivideAndConquerUsingRecursiveLoop() }
+    println("took ${howSlowIsDivideAndConquer / 1000} microseconds to execute!")
 }
